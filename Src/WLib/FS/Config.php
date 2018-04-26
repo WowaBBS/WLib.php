fd8ea2f852e1228c4b8fca1b85e2f8f26859876e
wowa69 <wowabbs+github@gmail.com>
2018-04-26 19:11:27 +0400

    ->LF

--- Src/WLib/FS/Config.php
+++ Src/WLib/FS/Config.php
@@ -1,1 +1,40 @@
-<? class T_FS_Config {   Var $List =[];   Var $Def  =[];   Function Get($Path)   {     $Path=StrToLower($Path);     $Paths=[];     $Res=$this->_Get($Paths, $Path);     ForEach($Paths as $Dir) // Add to cache       $List[$Dir]=$Res;     return $Res;   }      Function _Get(&$Paths, $Path)   {     $List=&$this->List;     if(IsSet($List[$Path])) return $Required[$Path];            $Dir=GetDir($Path);   //echo '!!!', $Dir, '-', $Path, "\n";     if(IsSet($List[$Dir.'*'])) return $List[$Dir.'*'];     if(IsSet($List[$Dir    ])) return $List[$Dir    ];     $Paths[]=$Dir;     while(true)     {       $Dir=GetDir($Dir);       if($Dir==='')         break;       if(IsSet($List[$Dir.'*/'])) return $List[$Dir.'*/'];       if(IsSet($List[$Dir     ])) return $List[$Dir     ];       $Paths[]=$Dir;     }     return $this->Def;   } }?>
+<?
+ class T_FS_Config
+ {
+   Var $List =[];
+   Var $Def  =[];
+
+   Function Get($Path)
+   {
+     $Path=StrToLower($Path);
+     $Paths=[];
+     $Res=$this->_Get($Paths, $Path);
+     ForEach($Paths as $Dir) // Add to cache
+       $List[$Dir]=$Res;
+     return $Res;
+   }
+   
+   Function _Get(&$Paths, $Path)
+   {
+     $List=&$this->List;
+     if(IsSet($List[$Path])) return $Required[$Path];
+       
+     $Dir=GetDir($Path);
+   //echo '!!!', $Dir, '-', $Path, "\n";
+     if(IsSet($List[$Dir.'*'])) return $List[$Dir.'*'];
+     if(IsSet($List[$Dir    ])) return $List[$Dir    ];
+     $Paths[]=$Dir;
+     while(true)
+     {
+       $Dir=GetDir($Dir);
+       if($Dir==='')
+         break;
+       if(IsSet($List[$Dir.'*/'])) return $List[$Dir.'*/'];
+       if(IsSet($List[$Dir     ])) return $List[$Dir     ];
+       $Paths[]=$Dir;
+     }
+     return $this->Def;
+   }
+ }
+
+?>

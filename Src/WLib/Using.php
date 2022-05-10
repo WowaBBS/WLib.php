7ccb044a56d538f3833ac0b46800ecc46c1d84a7
Wowa <wowabbs@gmail.com>
2022-05-10 21:38:08 +0400

    Updated Usimg.php

--- Src/WLib/Using.php
+++ Src/WLib/Using.php
@@ -1,6 +1,10 @@
 <?
- Include_Once 'Object/Loader.phpc';
- 
- $Loader=New C_Object_Loader();
+ if(!IsSet($Loader))
+ {
+   If(!Class_Exists('C_Object_Loader'))
+     Include_Once 'Object/Loader.phpc';
+   
+   $Loader=New C_Object_Loader();
+ }
  $Loader->Loader_Init(__DIR__);
 ?>
